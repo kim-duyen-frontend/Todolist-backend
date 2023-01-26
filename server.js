bf17d742d5c254d5f56c00ce1c7d7cc2a5bb22b4
Ngô Thị Kim Duyên <92040472+kim-duyen-frontend@users.noreply.github.com>
2023-01-26 12:18:59 +0700

    Update server.js
    adjust config cors

--- server.js
+++ server.js
@@ -12,7 +12,27 @@ mongoose.connect((process.env.MONGODB_URL),() => {
     console.log("Connected to MongoDB");
 })
 app.use(bodyParser.json({limit:"50mb"}));
-app.use(cors());
+//app.use(cors());
+app.use(function (req, res, next) {
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', "http://localhost:3000");
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Authorization');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    if (req.method === 'OPTIONS') {
+        return res.sendStatus(200);
+    }
+    // Pass to next layer of middleware
+    next();
+});
 app.use(morgan("common"));
 
 //ROUTES
